ac73fb5fa12a3c165852ac84aa56fe180141051b
lucasmathcode <164958181+lucasmathcode@users.noreply.github.com>
2024-11-27 14:05:15 -0300

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css"; // Se você estiver usando CSS
-import App from "./App"; // Importa o componente App principal
+import "./index.css"; 
+import App from "./App"; 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
